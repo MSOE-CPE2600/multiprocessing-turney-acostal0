b94a58d13ec583278f5897215789d95736fc4c6b
Lizbeth <acostal@msoe.edu>
2024-11-24 19:17:48 -0600

    Added mandelmovie.c

---
+++ mandelmovie.c
@@ -0,0 +1,9 @@
+/*********************************************
+ * Filename: mandelmovie.c
+ * Assignment: Lab 11
+ * Description: 
+ * Author: Lizbeth Acosta
+ * Date: 11/19/24
+ * Note: 
+ * 
+ *********************************************/
